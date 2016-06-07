14ab527aa7082ea8b867dfd4d53f6681205f64bf
christian otkjær <christian.otkjaer@gmail.com>
2016-06-07 14:44:02 +0200

    Moved test frameworks to cart file.private

---
+++ carthage_update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  cartahge_update.sh
+#  Data
+#
+#  Created by Christian Otkjær on 07/06/16.
+#  Copyright © 2016 Christian Otkjær. All rights reserved.
+
+carthage update --platform iOS
